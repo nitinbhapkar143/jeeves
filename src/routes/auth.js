faedb8580ded706b0b0033bb2a4cfd3114af4cc9
Nitin Bhapkar <nitin.bhapkar@datagamz.com>
2021-10-10 19:23:01 +0530

    initial setup of API documentation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const validator = require("../middleware/validator")
 
+/**
+ * @api {post} /api/auth/signup Signup
+ * @apiVersion 0.0.1
+ * @apiName Signup
+ * @apiGroup Auth
+ * @apiDescription Signup.
+ * 
+ */
+
 router.post('/records', validator.validateSignup(), async (req, res, next) => {
   try{    
   }catch(err){
